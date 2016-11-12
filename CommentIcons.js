95d209a9ba433cba3bacf061db892f6d5d8f084d
HyperFlash-Studios <hypersonicak@gmail.com>
2016-11-11 23:34:03 -0600

    Update CommentIcons.js

--- CommentIcons.js
+++ CommentIcons.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 $('li[data-user="HyperFlash Studios"] > .speech-bubble-avatar').append('<div class="councilcom">Councilor</div>');
-  $('li[data-user="Alemas2005"] > .speech-bubble-avatar').append('<div class="burocom">Bureaucrat</div>');
-  $('li[data-user="AmazingPythor"] > .speech-bubble-avatar').append('<div class="sysopcom">Administrator</div>');
-  $('li[data-user="Slicer Vorzakh"] > .speech-bubble-avatar').append('<div class="dmcom">Discussions Moderator</div>');
+  $('li[data-user="Alemas2005"], li[data-user="Riolu777"] > .speech-bubble-avatar').append('<div class="burocom">Bureaucrat</div>');
+  $('li[data-user="AmazingPythor"], li[data-user="Loney 97"] > .speech-bubble-avatar').append('<div class="sysopcom">Administrator</div>');
+  $('li[data-user="Slicer Vorzakh"], li[data-user="Impossibubbles"] > .speech-bubble-avatar').append('<div class="dmcom">Discussions Moderator</div>');
 });
