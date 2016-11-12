7a3013e83eb9adeef747aafb8b7d5db940a1762b
HyperFlash-Studios <hypersonicak@gmail.com>
2016-11-11 23:18:11 -0600

    Update CommentIcons.js

--- CommentIcons.js
+++ CommentIcons.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
 $('li[data-user="HyperFlash Studios"] > .speech-bubble-avatar').append('<div class="hypecom">Councilor</div>');
-});
-
-$(document).ready(function() {
   $('li[data-user="Alemas2005"] > .speech-bubble-avatar').append('<div class="burocom">Bureaucrat</div>');
+  $('li[data-user="Loney 97"] > .speech-bubble-avatar').append('<div class="sysopcom">Administrator</div>');
+  $('li[data-user="Slicer Vorzakh"] > .speech-bubble-avatar').append('<div class="dmcom">Discussions Moderator</div>');
 });
