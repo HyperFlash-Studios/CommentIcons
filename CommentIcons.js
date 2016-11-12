e6907ea68316ccfc9977d1cd5250731f46399092
HyperFlash-Studios <hypersonicak@gmail.com>
2016-11-11 19:53:47 -0600

    Update and rename Test script.js to CommentIcons.js

--- CommentIcons.js
+++ CommentIcons.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
 $('li[data-user="HyperFlash Studios"] > .speech-bubble-avatar').append('<div class="hypecom">Councilor</div>');
 });
+
+$(document).ready(function() {
+  $('li[data-user="Alemas2005"] > .speech-bubble-avatar').append('<div class=burocom">Bureaucrat</div>);
+});
