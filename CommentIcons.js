80433a65bfbc5fda045e485ea39350c6ff2bc295
Samantha Nguyen <SamanthaNguyen@users.noreply.github.com>
2016-11-12 11:29:21 -0600

    tidying up code, adding base class

--- CommentIcons.js
+++ CommentIcons.js
@@ -1,9 +1,9 @@
-$(document).ready(function() {
-	  $('li[data-user="Brandon Rhea"] > .speech-bubble-avatar, li[data-user="Sactage"] > .speech-bubble-avatar').append('<div class="staffcom">Staff</div>');
-	  $('li[data-user="Yatalu"] > .speech-bubble-avatar, li[data-user="Leviathan 89"] > .speech-bubble-avatar').append('<div class="helpercom">Helper</div>');
-	  $('li[data-user="Dark Yada"] > .speech-bubble-avatar, li[data-user="Speedit"] > .speech-bubble-avatar').append('<div class="vanguardcom">Vanguard</div>');
-	  $('li[data-user="HyperFlash Studios"] > .speech-bubble-avatar, li[data-user="Original Authority"] > .speech-bubble-avatar').append('<div class="councilcom">Councilor</div>');
-	  $('li[data-user="Alemas2005"] > .speech-bubble-avatar, li[data-user="Riolu777"] > .speech-bubble-avatar').append('<div class="burocom">Bureaucrat</div>');
-	  $('li[data-user="AmazingPythor"] > .speech-bubble-avatar, li[data-user="Loney 97"] > .speech-bubble-avatar').append('<div class="sysopcom">Administrator</div>');
-	  $('li[data-user="Slicer Vorzakh"] > .speech-bubble-avatar, li[data-user="Impossibubbles"] > .speech-bubble-avatar').append('<div class="dmcom">Discussions Moderator</div>');
-});
+$( document ).ready( function() {
+	  $('li[data-user="Brandon Rhea"] > .speech-bubble-avatar, li[data-user="Sactage"] > .speech-bubble-avatar').append('<div class="group-tag staffcom">Staff</div>');
+	  $('li[data-user="Yatalu"] > .speech-bubble-avatar, li[data-user="Leviathan 89"] > .speech-bubble-avatar').append('<div class="group-tag helpercom">Helper</div>');
+	  $('li[data-user="Dark Yada"] > .speech-bubble-avatar, li[data-user="Speedit"] > .speech-bubble-avatar').append('<div class="group-tag vanguardcom">Vanguard</div>');
+	  $('li[data-user="HyperFlash Studios"] > .speech-bubble-avatar, li[data-user="Original Authority"] > .speech-bubble-avatar').append('<div class="group-tag councilcom">Councilor</div>');
+	  $('li[data-user="Alemas2005"] > .speech-bubble-avatar, li[data-user="Riolu777"] > .speech-bubble-avatar').append('<div class="group-tag burocom">Bureaucrat</div>');
+	  $('li[data-user="AmazingPythor"] > .speech-bubble-avatar, li[data-user="Loney 97"] > .speech-bubble-avatar').append('<div class="group-tag sysopcom">Administrator</div>');
+	  $('li[data-user="Slicer Vorzakh"] > .speech-bubble-avatar, li[data-user="Impossibubbles"] > .speech-bubble-avatar').append('<div class="group-tag dmcom">Discussions Moderator</div>');
+} );
