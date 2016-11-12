ce19058eb3eb3e8a140a8f810251305e1630637c
HyperFlash-Studios <hypersonicak@gmail.com>
2016-11-12 09:45:12 -0600

    Update CommentIcons.js

--- CommentIcons.js
+++ CommentIcons.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
-$('li[data-user="HyperFlash Studios"] > .speech-bubble-avatar').append('<div class="councilcom">Councilor</div>');
+  $('li[data-user="Brandon Rhea"], li[data-user="Sactage"] > .speech-bubble-avatar').append('<div class="staffcom">Staff</div>');
+  $('li[data-user="Yatalu"], li[data-user="Leviathan 89"] > .speech-bubble-avatar').append('<div class="helpercom">Helper</div>');
+  $('li[data-user="Dark Yada"], li[data-user="Speedit"] > .speech-bubble-avatar').append('<div class="vanguardcom">Vanguard</div>');
+  $('li[data-user="HyperFlash Studios"], li[data-user="Original Authority"] > .speech-bubble-avatar').append('<div class="councilcom">Councilor</div>');
   $('li[data-user="Alemas2005"], li[data-user="Riolu777"] > .speech-bubble-avatar').append('<div class="burocom">Bureaucrat</div>');
   $('li[data-user="AmazingPythor"], li[data-user="Loney 97"] > .speech-bubble-avatar').append('<div class="sysopcom">Administrator</div>');
   $('li[data-user="Slicer Vorzakh"], li[data-user="Impossibubbles"] > .speech-bubble-avatar').append('<div class="dmcom">Discussions Moderator</div>');
