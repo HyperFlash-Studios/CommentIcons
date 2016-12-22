01d72d034da19908e2279dc18f36a714b77a4888
Veralann <hypersonicak@gmail.com>
2016-12-22 02:54:21 -0600

    Removing non-functional code

--- CommentIcons.js
+++ CommentIcons.js
@@ -1,4 +1,3 @@
-$( document ).ready( function() {
 	$( 'li[data-user="Brandon Rhea"] > .speech-bubble-avatar, li[data-user="Sactage"] > .speech-bubble-avatar' ).append( '<div class="group-tag staffcom">Staff</div>' );
 	$( 'li[data-user="Yatalu"] > .speech-bubble-avatar, li[data-user="Leviathan 89"] > .speech-bubble-avatar' ).append( '<div class="group-tag helpercom">Helper</div>' );
 	$( 'li[data-user="Dark Yada"] > .speech-bubble-avatar, li[data-user="Speedit"] > .speech-bubble-avatar' ).append( '<div class="group-tag vanguardcom">Vanguard</div>' );
@@ -6,4 +5,3 @@ $( document ).ready( function() {
 	$( 'li[data-user="Alemas2005"] > .speech-bubble-avatar, li[data-user="Riolu777"] > .speech-bubble-avatar' ).append( '<div class="group-tag burocom">Bureaucrat</div>' );
 	$( 'li[data-user="AmazingPythor"] > .speech-bubble-avatar, li[data-user="Loney 97"] > .speech-bubble-avatar' ).append( '<div class="group-tag sysopcom">Administrator</div>' );
 	$( 'li[data-user="Slicer Vorzakh"] > .speech-bubble-avatar, li[data-user="Impossibubbles"] > .speech-bubble-avatar' ).append( '<div class="group-tag dmcom">Discussions Moderator</div>' );
-} );
